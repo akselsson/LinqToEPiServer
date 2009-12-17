a14c961893be182699f826aa3acf0278e8f0cda2
unknown <Patrik@.(none)>
2009-12-17 14:38:49 +0700

    databas som fungerar med sql server 2005

--- src/LinqToEPiServer.Tests/Helpers/IntegrationTestDatabase.cs
+++ src/LinqToEPiServer.Tests/Helpers/IntegrationTestDatabase.cs
@@ -12,18 +12,18 @@ namespace LinqToEPiServer.Tests.Helpers
 RESTORE DATABASE [LinqToEPiServer_IntegrationTests] 
 FROM  DISK = N'{0}\LinqToEPiServer_IntegrationTests.bak' 
 WITH  FILE = 1,  
-MOVE N'Finnish_Swedish_CI_AS_dat' TO N'{0}\LinqToEPiServer_IntegrationTests.mdf',  
-MOVE N'Finnish_Swedish_CI_AS_log' TO N'{0}\LinqToEPiServer_IntegrationTests_1.ldf',  
+MOVE N'LinqToEPiServer_IntegrationTests' TO N'{0}\LinqToEPiServer_IntegrationTests.mdf',  
+MOVE N'LinqToEPiServer_IntegrationTests_log' TO N'{0}\LinqToEPiServer_IntegrationTests_1.ldf',  
 NOUNLOAD,  
 STATS = 10
 ";
 
-        private const string MasterConnectionString = @"Data Source=.;integrated security=true;initial catalog=master";
+        private const string MasterConnectionString = @"Data Source=.\sqlexpress;integrated security=true;initial catalog=master";
 
         private static readonly DirectoryInfo DbFolder = new DirectoryInfo("DB");
 
         public const string ConnectionString =
-     @"Data Source=.;integrated security=true;MultipleActiveResultSets=True;Initial Catalog=LinqToEPiServer_IntegrationTests";
+     @"Data Source=.\sqlexpress;integrated security=true;MultipleActiveResultSets=True;Initial Catalog=LinqToEPiServer_IntegrationTests";
 
         public static void Restore()
         {
