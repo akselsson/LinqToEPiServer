e36daf67488f5bba22dc35a46298e1a06d2cc96e
unknown <patrik.akselsson@.se.valtech.com>
2010-01-08 23:13:24 +0100

    Refactoring

--- src/LinqToEPiServer.Tests/IntegrationTests/IntegrationTestsBase.cs
+++ src/LinqToEPiServer.Tests/IntegrationTests/IntegrationTestsBase.cs
@@ -23,19 +23,49 @@ namespace LinqToEPiServer.Tests.IntegrationTests
         protected override void before_each_test()
         {
             base.before_each_test();
+            StartTransaction();
+            RunAsAdmin();
+            InitPageDefinitions();
+            InitPageTypeBuilder();
+        }
+
+        private void StartTransaction()
+        {
             _transaction = new TransactionScope(TransactionScopeOption.RequiresNew, TimeSpan.FromMinutes(20));
+        }
+
+        private void RunAsAdmin()
+        {
             _originalPrincipal = Thread.CurrentPrincipal;
-            Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity("admin"), new[] {"administrators"});
+            Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity("admin"), new[] { "administrators" });
+        }
+
+        private static void InitPageDefinitions()
+        {
             PluginHelper.LocatePluginsFromReferencedAssemblies();
             PageDefinitionTypePlugInAttribute.Start();
+        }
+
+        private static void InitPageTypeBuilder()
+        {
             PageTypeBuilderHelper.Init();
         }
 
         protected override void after_each_test()
         {
-            Thread.CurrentPrincipal = _originalPrincipal;
-            _transaction.Dispose();
+            ResetPrincipal();
+            RollbackTransaction();
             base.after_each_test();
+        }
+
+        private void ResetPrincipal()
+        {
+            Thread.CurrentPrincipal = _originalPrincipal;
+        }
+
+        private void RollbackTransaction()
+        {
+            _transaction.Dispose();
         }
     }
 }
